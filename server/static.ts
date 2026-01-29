be5b05f88a81d621dba45fdd987d191314f57319
RADHAD <radha.d@mercedes-benz.com>
2026-01-29 16:47:11 +0530

    Update application files

--- server/static.ts
+++ server/static.ts
@@ -1,6 +1,9 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
@@ -13,7 +16,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
+  app.use("/*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
